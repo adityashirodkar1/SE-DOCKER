9ba2158de304d021740b99b631151feb589c7b72
Aditya <mailforcoding86@gmail.com>
2023-11-26 17:10:03 +0530

    second commit

--- db.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-connection_string = "mongodb+srv://adishiro:gintoki@cluster0.wlmssnp.mongodb.net/SE?retryWrites=true&w=majority"
-client = MongoClient(connection_string)
-db = client.get_database("SE")
-users = db.get_collection("users")
